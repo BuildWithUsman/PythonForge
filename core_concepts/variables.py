00dc9b8daf0decae5b2d14a661ad0b1001f0c0a7
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-02 00:24:29 -0500

    Adding structure to Repo

---
+++ core_concepts/variables.py
@@ -0,0 +1,9 @@
+# Variables = A container for a value (string, integer, float, boolean)
+# A Variable behaves as if it was the value it contains
+
+first_name = "Bro"
+food = "pizza"
+
+print(first_name)
+print(food)
+
