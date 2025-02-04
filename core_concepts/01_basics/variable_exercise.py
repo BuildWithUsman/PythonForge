68b168c5473dbcd0c87fd87ae5c919d3949868b7
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-03 23:56:04 -0500

    Forgot to add what type of variables I was using for each line

--- core_concepts/01_basics/variable_exercise.py
+++ core_concepts/01_basics/variable_exercise.py
@@ -9,13 +9,13 @@ pi = 3.1415926535
 
 work_in_tech = True 
 
-print(f"My name is: {name}")
+print(f"My name is: {name} and this is a String Type variable")
 
-print(f"The current year is: {current_year}")
+print(f"The current year is: {current_year} and this is a Integer Type variable")
 
-print(f"The first 10 numbers of Pi are: {pi}")
+print(f"The first 10 numbers of Pi are: {pi} and this is a Float Type variable")
 
 if work_in_tech:
-    print(f"You currently work in IT")
+    print(f"You currently work in IT and this is a Boolean Type variable")
 else:
-    print(f"You currently DO NOT work in IT")
+    print(f"You currently DO NOT work in IT and this is a Boolean Type variable")
