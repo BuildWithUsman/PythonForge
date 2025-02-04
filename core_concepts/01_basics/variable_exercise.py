a5c789629f2e8a3c972a2caa90d7b67fb1090533
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-03 23:53:17 -0500

    Simple variable exercise

---
+++ core_concepts/01_basics/variable_exercise.py
@@ -0,0 +1,21 @@
+# For this variable exercise, enter your name, current year, first 10 numbers of Pi, and if you work in IT.
+# Furthermore, print out the variables and describle what type of variables are they are. 
+
+name = "John"
+
+current_year = 2025
+
+pi = 3.1415926535
+
+work_in_tech = True 
+
+print(f"My name is: {name}")
+
+print(f"The current year is: {current_year}")
+
+print(f"The first 10 numbers of Pi are: {pi}")
+
+if work_in_tech:
+    print(f"You currently work in IT")
+else:
+    print(f"You currently DO NOT work in IT")
