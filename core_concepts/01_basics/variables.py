9b69fa8731565d95ee7a7db7e98f5073a7a6ccfb
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-03 23:15:57 -0500

    Completed learning Strings

--- core_concepts/01_basics/variables.py
+++ core_concepts/01_basics/variables.py
@@ -1,7 +1,16 @@
 # Variable(s) = A container for a value (string, integer, float, and boolean)
 # A Variable behaves as if it was the value it contains 
 
+# In this case, the variables below are called Strings.
+# Strings
 first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+# You can use this method to print out onto the terminal. 
+print(first_name) # You can use this method to print out onto the terminal. 
 
-print(first_name)
+# Or you can use the "f-string", with the "f-string" method you can write a Python expression between { and } characters that can refer to variables or literal values. 
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
 
