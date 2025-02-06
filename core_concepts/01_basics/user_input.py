e75a9643ea6ca42a77e98366d62a03234632ec9d
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-05 23:48:41 -0500

    Understanding how to concatenate string into a int when using input

--- core_concepts/01_basics/user_input.py
+++ core_concepts/01_basics/user_input.py
@@ -3,5 +3,20 @@
 
 #input() #Running this by itself will prompt the user to enter something but don't display any text to tell the user what to enter.
 
-input("What is your name?: ")
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+# age = age + 1 If you run this code, it will error because of the following error message:
+#               TypeError: can only concatenate str (not "int") to str
+#               To fix this error, we need to type cast the string into a integer since we can only do arithmetric expressions with ints or floats.
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name}")
+print("Happy Birthday")
+print(f"You are {age} years old")
+
+
+
 
