16d22b970d8cd17e7319e758a99c16ff5c5d4f64
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-05 23:39:45 -0500

    First user input value

--- core_concepts/01_basics/user_input.py
+++ core_concepts/01_basics/user_input.py
@@ -1,3 +1,7 @@
 # input() = A function that prompts the user to enter data. 
 #           Returns the entered data as a string. 
 
+#input() #Running this by itself will prompt the user to enter something but don't display any text to tell the user what to enter.
+
+input("What is your name?: ")
+
