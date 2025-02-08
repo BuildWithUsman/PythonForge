b1e46726bd07ac7a726d19b5eb73ae973067b597
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-07 23:58:12 -0500

    Subtraction

--- core_concepts/01_basics/arithmetic_and_math_expressions.py
+++ core_concepts/01_basics/arithmetic_and_math_expressions.py
@@ -1,7 +1,12 @@
 friends = 0 
 
+# Addtion
 # friends = friends + 1
 # OR 
-friends += 1
+# friends += 1 - This is known as augmented assigment operator, less text and easier to add
 
+# Subtraction
+# friends = friends -2 
+# OR 
+friends -= 2
 print(friends)
