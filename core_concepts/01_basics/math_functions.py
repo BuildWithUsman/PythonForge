19feffce02a63d1de5bece7165c5ebccbc5dc77c
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-08 23:46:24 -0500

    Covered Math Functions

--- core_concepts/01_basics/math_functions.py
+++ core_concepts/01_basics/math_functions.py
@@ -1,14 +1,24 @@
+import math
 # In this file, we will cover some built in math related functions. 
 
 x = 3.14
 y = 4
 z = 5
 z1 = -5
+z2 = 9
 
 # result = round(x) # Round number to the nearest whole number. 
 # result = abs(z1) # Absolute value of number, -5 is going to be 5. 
 # result = pow(4, 3) # Power function, 4 to the power of 3. 
 # result = max(x, y, z) # The largest value from the range available.
 # result = min(x, y, z) # The smallest value from the range available.
+# print(result)
 
-print(result)
+# Useful Math Constants and Functions for Math, to use them we will need to import the math module.
+
+# print(math.pi) # Use this to print the Pi value
+# print(math.e) # Use this to print the expontential value
+# result = math.sqrt(z2) # Use this to get the square root of a value. 
+# result = math.ceil(x) # Round the number up to the nearest whole number. 
+# result = math.floor(x) # Round the number down to the nearest whole number.
+# print(result)
