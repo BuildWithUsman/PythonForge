e099c496e890a7f63091c1fad79fa43f7ce4dcdb
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-08 23:58:28 -0500

    Area Exercise

--- core_concepts/01_basics/math_exercises.py
+++ core_concepts/01_basics/math_exercises.py
@@ -1,9 +1,20 @@
 # Exercise 1 - Print the Circumference (Formula is C = 2πr)
 
+# import math
+
+# radius = float(input("Enter the radius of a circle: "))
+
+# circumference = 2 * math.pi * radius
+
+# print(f"The circumference is: {round(circumference, 2)}cm")
+
+
+# Exercise 2 - Print the Area of a Circle (Formula is A = πr²)
+
 import math
 
 radius = float(input("Enter the radius of a circle: "))
 
-circumference = 2 * math.pi * radius
+area = math.pi * pow(radius, 2)
 
-print(f"The circumference is: {round(circumference, 2)}cm")
+print(f"The area of the circle is: {round(area, 2)}cm^2")
