9d2362fdb7ba1517c08cd324723751f8ccfd2662
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-08 23:39:26 -0500

    Covered Math Functions

---
+++ core_concepts/01_basics/math_functions.py
@@ -0,0 +1,14 @@
+# In this file, we will cover some built in math related functions. 
+
+x = 3.14
+y = 4
+z = 5
+z1 = -5
+
+# result = round(x) # Round number to the nearest whole number. 
+# result = abs(z1) # Absolute value of number, -5 is going to be 5. 
+# result = pow(4, 3) # Power function, 4 to the power of 3. 
+# result = max(x, y, z) # The largest value from the range available.
+# result = min(x, y, z) # The smallest value from the range available.
+
+print(result)
