847904ccfa2d354901b7fb53743966025ed3f6a8
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-09 23:59:24 -0500

    Name example

--- core_concepts/01_basics/if_statements.py
+++ core_concepts/01_basics/if_statements.py
@@ -4,18 +4,29 @@
 # Age/Card Sign Up Program
 # age = int(input("Enter your age: "))
 
-#if age >= 100:
+# if age >= 100:
 #    print("You are too old to sign up!")
-#elif age >= 18:
+# elif age >= 18:
 #    print("You are now signed up!")
-#elif age < 0:
+# elif age < 0:
 #    print("You haven't been born yet!")
-#else:
+# else:
 #    print("You must be 18+ to sign up!")    
 
-response = input("Would you like food? (Y/N): ")
+# Food Example
 
-if response = "Y": # == is the comparsion operator, while the = is the assign operator. 
-    print("Have some food!")
+# response = input("Would you like food? (Y/N): ")
+
+# if response = "Y": # == is the comparsion operator, while the = is the assign operator. 
+#     print("Have some food!")
+# else:
+#     print("No food for you")
+
+# Name Example 
+
+name = input("Enter your name: ")
+
+if name == "":
+    print("You did not type in your name!")
 else:
-    print("No food for you")
+    print(f"Hello {name}")
