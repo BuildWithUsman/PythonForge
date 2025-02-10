f12463a755e55eeb116912b82f70b008fbafbd03
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-09 23:55:32 -0500

    Comparsion Operator

--- core_concepts/01_basics/if_statements.py
+++ core_concepts/01_basics/if_statements.py
@@ -1,15 +1,21 @@
 # if = Do some code only IF some codition is True 
 #      Else do something else.
 
-age = int(input("Enter your age: "))
+# Age/Card Sign Up Program
+# age = int(input("Enter your age: "))
 
-if age >= 100:
-    print("You are too old to sign up!")
-elif age >= 18:
-    print("You are now signed up!")
-elif age < 0:
-    print("You haven't been born yet!")
+#if age >= 100:
+#    print("You are too old to sign up!")
+#elif age >= 18:
+#    print("You are now signed up!")
+#elif age < 0:
+#    print("You haven't been born yet!")
+#else:
+#    print("You must be 18+ to sign up!")    
+
+response = input("Would you like food? (Y/N): ")
+
+if response = "Y": # == is the comparsion operator, while the = is the assign operator. 
+    print("Have some food!")
 else:
-    print("You must be 18+ to sign up!")    
-
-
+    print("No food for you")
