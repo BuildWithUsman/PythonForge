fecdf51e284ccdc0cbfde4afd34c8fa43a0a796e
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-09 23:44:52 -0500

    Basic if statement, checking one codition

--- core_concepts/01_basics/if_statements.py
+++ core_concepts/01_basics/if_statements.py
@@ -1,3 +1,11 @@
 # if = Do some code only IF some codition is True 
 #      Else do something else.
 
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are now signed up!")
+else:
+    print("You must be 18+ to sign up!")    
+
+
