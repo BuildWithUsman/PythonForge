449ffd3d5aca05510a929e1574267eb6e793b962
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-10 23:51:22 -0500

    Compelted Program

--- projects/calculator_program.py
+++ projects/calculator_program.py
@@ -3,3 +3,18 @@
 operator = input("Enter an operator (+, -, *, or /): ")
 num1 = float(input("Enter the 1st Number: "))
 num2 = float(input("Enter the 2nd Number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*": 
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else: 
+    print("Please Enter a valid operator")
