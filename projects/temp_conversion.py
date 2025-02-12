bdc102ee0a4c79e61c75fedccfc38ef376683c29
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-11 23:54:02 -0500

    Celsius part is working

--- projects/temp_conversion.py
+++ projects/temp_conversion.py
@@ -4,7 +4,8 @@ unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
 temp = float(input("Enter the temperature: "))
 
 if unit == "C": 
-    pass
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temperature in Fahrenheit is: {temp}°F")
 elif unit == "F":
     pass
 else:
