f75ad68ff7deaa138a2449932bc8d1d8b3a1c491
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-12 23:26:50 -0500

    Worked with the AND operator

--- core_concepts/01_basics/logical_operators.py
+++ core_concepts/01_basics/logical_operators.py
@@ -3,11 +3,25 @@
 #                     and = both conditions must be True  
 #                     not = inverts the condition (not False, not True)
 
-temp = 36
-is_raining = False 
+# # OR Operator
+# temp = 36
+# is_raining = False 
 
-if temp > 35 or temp < 0 or is_raining:
-    print("The outdoor event is cancelled")
-else: 
-    print("The outdoor event is still scheduled")
+# if temp > 35 or temp < 0 or is_raining:
+#     print("The outdoor event is cancelled")
+# else: 
+#     print("The outdoor event is still scheduled")
 
+# AND Operator 
+temp = 15
+is_sunny = True
+
+if temp >= 28 and is_sunny:
+    print("It is HOT outside")
+    print("It is Sunny")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside")
+    print("It is sunny")
+elif 28 > temp > 0 and is_sunny:
+    print("It is WARM outside")
+    print("It is Sunny")
