b8764a2382b9954745b3f0661b42ec782ff9818b
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-12 23:15:18 -0500

    Used the OR operator

---
+++ core_concepts/01_basics/logical_operators.py
@@ -0,0 +1,13 @@
+# Logical Operators = Evaluate multiple conditions (or, and, not)
+#                     or = at least one condition must be True. 
+#                     and = both conditions must be True  
+#                     not = inverts the condition (not False, not True)
+
+temp = 36
+is_raining = False 
+
+if temp > 35 or temp < 0 or is_raining:
+    print("The outdoor event is cancelled")
+else: 
+    print("The outdoor event is still scheduled")
+
