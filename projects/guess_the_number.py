dc71c29198931e968f4049942741c0ae6f722be6
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-13 23:42:22 -0500

    Progess

---
+++ projects/guess_the_number.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+number = random.randint(1, 50) # Generate a random number from 1 to 50
+print(number) # Print that number
+
+print("We are going to play a game called Guess The Number")
+guess = int(input("Please a number betweeen 1 to 50: "))
+
+print(type(guess))
+
+if guess == number:
+    print("Great job, you are correct!")
+elif guess > number:
+    print("You are too high!")
+elif guess < number:
+    print("You are too low!")
+else:
+    pass
