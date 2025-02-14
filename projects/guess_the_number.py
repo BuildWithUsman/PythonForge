27d4c16749a991fc701a2836c0b2f80c4e776b84
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-13 23:48:43 -0500

    Changing range to 1 to 25

--- projects/guess_the_number.py
+++ projects/guess_the_number.py
@@ -1,21 +1,22 @@
 import math
 import random
 
-number = random.randint(1, 50) # Generate a random number from 1 to 50
+number = random.randint(1, 25) # Generate a random number from 1 to 50
 print(number) # Print that number
 
 print("We are going to play a game called Guess The Number")
-guess = int(input("Please a number betweeen 1 to 50: "))
+guess = int(input("Please a number betweeen 1 to 25: "))
 
 print(type(guess))
 
-if guess == number or new_guess == number:
+if guess == number:
     print("Great job, you are correct!")
 elif guess > number:
     print("You are too high!")
-    new_guess = int(input("Please a number betweeen 1 to 50: "))
+    new_guess = int(input("Please a number betweeen 1 to 25: "))
+
 elif guess < number:
     print("You are too low!")
-    new_guess = int(input("Please a number betweeen 1 to 50: "))
+    new_guess = int(input("Please a number betweeen 1 to 25: "))
 else:
     pass
