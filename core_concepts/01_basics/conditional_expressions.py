a78523b7a9bc0e3ecdcf54dfa47113fcbc89e695
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-14 23:54:10 -0500

    Basic conditional expression

--- core_concepts/01_basics/conditional_expressions.py
+++ core_concepts/01_basics/conditional_expressions.py
@@ -2,3 +2,6 @@
 #                          Print or assign one of the two values based on a condition 
 #                          X if condition else Y
 
+num = 5
+
+print("Positive" if num > 0 else "Negative")
