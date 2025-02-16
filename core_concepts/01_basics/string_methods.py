8d9156ef44ba4ad50beb76fc62e08e7d6f834e22
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-15 23:55:44 -0500

    String Method Examples

---
+++ core_concepts/01_basics/string_methods.py
@@ -0,0 +1,11 @@
+name = input("Enter your full name: ")
+
+# result = len(name)
+# result = name.find("a")
+# result = name.rfind("a") # If the letter doesn't exist, it will return a -1. 
+# name = name.capitalize() # Capitalize the First Letter
+# name = name.upper() # Make all letters uppercase
+# name = name.lower() # Make all letters lowercase
+# result = name.isdigit() # Checks to see if the string ONLY contains digits, it will return True or False.
+result = name.isalpha() # Check to see if the string ONLY contains alphabets, it will return True or False. Numbers and White Spaces = False. 
+print(result)
