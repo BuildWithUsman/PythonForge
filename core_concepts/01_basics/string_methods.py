440afb9302f743aad935606c4cb6b03270dd9ee2
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-16 21:34:18 -0800

    Added comments for string methods

--- core_concepts/01_basics/string_methods.py
+++ core_concepts/01_basics/string_methods.py
@@ -1,14 +1,14 @@
 # name = input("Enter your full name: ")
-phone_number = input("Please Enter your Phone Number: ")
+# phone_number = input("Please Enter your Phone Number: ")
 
 # result = len(name)
-# result = name.find("a")
-# result = name.rfind("a") # If the letter doesn't exist, it will return a -1. 
+# result = name.find("a") # Find where the letter is located, starts from 0. 
+# result = name.rfind("a") # Reverse find where the letter is located, start from 0. If the letter doesn't exist, it will return a -1. 
 # name = name.capitalize() # Capitalize the First Letter
 # name = name.upper() # Make all letters uppercase
 # name = name.lower() # Make all letters lowercase
 # result = name.isdigit() # Checks to see if the string ONLY contains digits, it will return True or False.
 # result = name.isalpha() # Check to see if the string ONLY contains alphabets, it will return True or False. Numbers and White Spaces = False. 
-# result = phone_number.count("-")
-phone_number = phone_number.replace("-", " ")
-print(phone_number)
+# result = phone_number.count("-") # Count how many times the character "-" shows up.
+# phone_number = phone_number.replace("-", " ") # Find and Replace "-" with a white space " ".
+# print(phone_number)
