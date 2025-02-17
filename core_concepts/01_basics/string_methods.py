03a8185ea14fe2ab92843f39b07237d64f79f96d
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-16 21:48:33 -0800

    Completed Final part of the exercise

--- core_concepts/01_basics/string_methods.py
+++ core_concepts/01_basics/string_methods.py
@@ -2,7 +2,7 @@
 # phone_number = input("Please Enter your Phone Number: ")
 
 # result = len(name)
-# result = name.find("a") # Find where the letter is located, starts from 0. 
+# result = name.find("a") # Find where the letter is located, starts from 0. Find Method looks for any character.
 # result = name.rfind("a") # Reverse find where the letter is located, start from 0. If the letter doesn't exist, it will return a -1. 
 # name = name.capitalize() # Capitalize the First Letter
 # name = name.upper() # Make all letters uppercase
@@ -14,18 +14,20 @@
 # print(phone_number)
 
 # Validate User Input Exercise 
-# 1. username is no more than 12 characters. Make the len() method with a if statement. 
-# 2. username must not contain spaces.  
+# 1. username is no more than 12 characters. Use the len() method with a if statement. 
+# 2. username must not contain spaces. Use the find() method with a if statement, remember if a character doesn't exist it will print -1. 
 # 3. username must not contain digits. 
 
 username = input("Enter a username: ")
 
-username.find(" ")
+username.isalpha() 
 
 if len(username) > 12:
     print("Your username can't be more than 12 characters")
 elif not username.find(" ") == -1:
     print("Your username can't contain spaces")
+elif not username.isalpha():
+    print("Your username can't contain digits")
 else:
     print(f"Welcome {username}")
 
