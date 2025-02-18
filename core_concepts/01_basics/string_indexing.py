5b88efc879ecf774ec28d822b63a4efb9a1fe779
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-17 22:12:58 -0800

    Basic Indexing Examples

---
+++ core_concepts/01_basics/string_indexing.py
@@ -0,0 +1,16 @@
+# Indexing = Accessing elements of a sequence using [] (indexing operator)
+#            [start : end : step] - 3 Fields we can fill in, start, end, and step. 
+#            If you have only field listed, without an colons it will assume you are filling in the starting position. 
+
+credit_number = "1234-5678-9012-3456"
+
+print(credit_number[0]) # The first position always has a index of 0. In this case, it will print out the first character which is 1. 
+
+print(credit_number[1]) # In this case, this will print out the second character which is 2. 
+
+print(credit_number[2]) # In this case, this will print out the third character which is 3. 
+
+print(credit_number[3]) # In this case, this will print out the fourth character which is 4. 
+
+print(credit_number[4]) # In this case, this will print out the fifth character which is a -. 
+
