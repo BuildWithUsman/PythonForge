5ebb94268715645dcc72479addd1d9270f34e612
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-17 22:37:47 -0800

    Step Field/Indexing Examples

--- core_concepts/01_basics/string_indexing.py
+++ core_concepts/01_basics/string_indexing.py
@@ -41,3 +41,6 @@ print(credit_number[-1:-5:-1]) # Indexing Slicing moves left to right by default
                                # Because by default, Python assumes a default step of +1, which results in a empty string. 
                                # To fix it, we need to declare a -1 step - [-1:-5:-1]
 
+# Using the Step field, we can access the characters in a string by a given step. We can count by 2s, 3s, and etc. 
+print(credit_number[::2]) # This will print every 2nd character in our string, Python is assuming everything from the Start to the End. 
+print(credit_number[::3]) # This will print every 3nd character in our string, Python is assuming everything from the Start to the End. 
