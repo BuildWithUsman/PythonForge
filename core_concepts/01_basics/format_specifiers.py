30d8dc3dc1b56b1530220f5ee54c7e743c9afc2b
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-18 23:28:40 -0500

    Format Specifier to Round To That Many Decimal Space

--- core_concepts/01_basics/format_specifiers.py
+++ core_concepts/01_basics/format_specifiers.py
@@ -12,4 +12,12 @@
 # :  = Insert a space before positive numbers
 # :, = Comma Separator 
 
-                          
+price1 = 3.14159
+price2 = -987.65
+price3 = 12.34
+
+# Using the .(number)f Format Specifier to Round to that many decimal places.
+print(f"Price 1 is ${price1:.3f}")
+print(f"Price 2 is ${price2:.3f}")
+print(f"Price 3 is ${price3:.3f}")
+
