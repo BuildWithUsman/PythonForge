820b2bc2289c7385c7502dc6dac0a4b17c066850
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-18 23:22:37 -0500

    Adding comments describing what Format Specifiers are

---
+++ core_concepts/01_basics/format_specifiers.py
@@ -0,0 +1,15 @@
+# Format Specifiers = {value:flags} format a value based on what flags are inserted. 
+#                               flags are inserted. 
+# 
+# .(number)f = Round to that many decimal places (fixed point)
+# :(number) = Allocate that many spaces
+# :03 = Allocate and zero pad that many spaces
+# :< = Left Justify 
+# :> = Right Justify 
+# :^ = Center Align 
+# :+ = Use a plus sign to indicate positive value
+# : = = Place sign to leftmost position
+# :  = Insert a space before positive numbers
+# :, = Comma Separator 
+
+                          
