9b66d7ee487552ce8652e9c8821ecefe19de1a31
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-19 23:06:47 -0800

    While Loop Age Example

--- core_concepts/01_basics/while_loops.py
+++ core_concepts/01_basics/while_loops.py
@@ -1,6 +1,6 @@
 # while loop = execute some code WHILE some condition remains true. 
 
-name = input("Enter your name: ")
+# name = input("Enter your name: ")
 
 # if name == "": 
 #     print("You did not enter your name")
@@ -9,8 +9,16 @@ name = input("Enter your name: ")
 
 # If we want the user to be continuously prompted for a input, we will use a While Loop 
 
-while name == "":
-    print("You did not enter your name")
-    name = input("Enter your name: ") # You need a exit statement or else, the while loop will never exit out. 
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name: ") # You need a exit statement or else, the while loop will never exit out. 
 
-print(f"Hello {name}")
+# print(f"Hello {name}")
+
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")
