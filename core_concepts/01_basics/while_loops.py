f73f07af418fa545ebc427ffc38953a5f1917f27
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-19 23:05:48 -0800

    While Loop Name Example

--- core_concepts/01_basics/while_loops.py
+++ core_concepts/01_basics/while_loops.py
@@ -1,2 +1,16 @@
 # while loop = execute some code WHILE some condition remains true. 
 
+name = input("Enter your name: ")
+
+# if name == "": 
+#     print("You did not enter your name")
+# else:
+#     print(f"Hello {name}")
+
+# If we want the user to be continuously prompted for a input, we will use a While Loop 
+
+while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name: ") # You need a exit statement or else, the while loop will never exit out. 
+
+print(f"Hello {name}")
