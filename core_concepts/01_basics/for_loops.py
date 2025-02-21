0537a916a6f2cfbf318ebd1c5bc7e083e9618951
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-20 23:33:30 -0800

    Example 4 - Iterate over a String

--- core_concepts/01_basics/for_loops.py
+++ core_concepts/01_basics/for_loops.py
@@ -12,6 +12,12 @@
 
 # print("Happy New Year!")
 
-#Example 3 - Printing numbers 1 to 10 but using the step function to skip numbers.
-for x in range(1, 11, 3): # To print the numbers 1 to 10 reversed you would use "reversed" and enclose it within the range.   
+# Example 3 - Printing numbers 1 to 10 but using the step function to skip numbers.
+# for x in range(1, 11, 3): # To print the numbers 1 to 10 reversed you would use "reversed" and enclose it within the range.   
+#     print(x)
+
+# Example 4 - Iterate over String 
+credit_card = "1234-5678-9012-3456"
+
+for x in credit_card: # X represents our current position, starting at 0 and progressing until the end of the string. 0 in our example is 1.
     print(x)
