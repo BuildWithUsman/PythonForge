9d806a4b26607d330c84019abeec391392f542a2
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-21 02:38:30 -0500

    Example 5 - Skip over Iteration using keyword, Continue.

--- core_concepts/01_basics/for_loops.py
+++ core_concepts/01_basics/for_loops.py
@@ -17,7 +17,14 @@
 #     print(x)
 
 # Example 4 - Iterate over String 
-credit_card = "1234-5678-9012-3456"
+# credit_card = "1234-5678-9012-3456"
 
-for x in credit_card: # X represents our current position, starting at 0 and progressing until the end of the string. 0 in our example is 1.
-    print(x)
+# for x in credit_card: # X represents our current position, starting at 0 and progressing until the end of the string. 0 in our example is 1.
+#     print(x)
+
+# Example 5 - Skip over a Iteration, for example if I want to skip the number 13 when printing 1 to 20. 
+for x in range(1, 21):
+    if x == 13:
+        continue # Continue is a keyword that is used inside of a for loop to skip a iteration of something. 
+    else:
+        print(x)
