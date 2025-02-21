9371008d4c3cdcc193622f6ecc7da6f72f6ae977
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-20 23:22:54 -0800

    Printing numbers 1 to 10 but reversed

--- core_concepts/01_basics/for_loops.py
+++ core_concepts/01_basics/for_loops.py
@@ -1,6 +1,11 @@
 # for loops = Execute a block of code for a fixed number of times. 
 #             You can iterate over a range, stirng, sequence, etc. 
 
-for x in range(1, 11): # Second number mentioned in the range is exlcusive, so if you want to print 1 to 10 your end number would 11.
-                       # X is the variable storing the value, you can also use counter instead of x as the variable. 
+# for x in range(1, 11): # Second number mentioned in the range is exlcusive, so if you want to print 1 to 10 your end number would 11.
+#                        # X is the variable storing the value, you can also use counter instead of x as the variable. 
+#     print(x)
+
+for x in reversed(range(1, 11)): # To print the numbers 1 to 10 reversed you would use "reversed" and enclose it within the range.   
     print(x)
+
+print("Happy New Year!")
