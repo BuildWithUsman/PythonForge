e90afc5160d7a18931a34a4f8819d8b7277df53b
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-20 23:27:05 -0800

    Example 3 - Printing numbers 1 to 10 but skipping by 3.

--- core_concepts/01_basics/for_loops.py
+++ core_concepts/01_basics/for_loops.py
@@ -1,11 +1,17 @@
 # for loops = Execute a block of code for a fixed number of times. 
 #             You can iterate over a range, stirng, sequence, etc. 
 
+# Example #1 - Printing numbers 1 to 10. 
 # for x in range(1, 11): # Second number mentioned in the range is exlcusive, so if you want to print 1 to 10 your end number would 11.
 #                        # X is the variable storing the value, you can also use counter instead of x as the variable. 
 #     print(x)
 
-for x in reversed(range(1, 11)): # To print the numbers 1 to 10 reversed you would use "reversed" and enclose it within the range.   
-    print(x)
+# Example #2 - Printing numbers 1 to 10 Reversed.
+# for x in reversed(range(1, 11)): # To print the numbers 1 to 10 reversed you would use "reversed" and enclose it within the range.   
+#     print(x)
 
-print("Happy New Year!")
+# print("Happy New Year!")
+
+#Example 3 - Printing numbers 1 to 10 but using the step function to skip numbers.
+for x in range(1, 11, 3): # To print the numbers 1 to 10 reversed you would use "reversed" and enclose it within the range.   
+    print(x)
