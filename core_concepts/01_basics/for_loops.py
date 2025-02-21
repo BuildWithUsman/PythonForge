50b8927552d9e522270d2a4604b33795481f91a0
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-20 23:20:19 -0800

    Basic For Loop Example, counting 1 to 10

--- core_concepts/01_basics/for_loops.py
+++ core_concepts/01_basics/for_loops.py
@@ -1,3 +1,6 @@
 # for loops = Execute a block of code for a fixed number of times. 
 #             You can iterate over a range, stirng, sequence, etc. 
 
+for x in range(1, 11): # Second number mentioned in the range is exlcusive, so if you want to print 1 to 10 your end number would 11.
+                       # X is the variable storing the value, you can also use counter instead of x as the variable. 
+    print(x)
