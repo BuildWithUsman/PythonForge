e6bc2fecbfd81ae8cdd268760c2a3ae3f71cc982
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-21 22:50:31 -0800

    Adding hours to digital counterdown timer

--- projects/countdown_timer.py
+++ projects/countdown_timer.py
@@ -5,8 +5,8 @@ my_time = int(input("Enter the time in seconds: "))
 for x in range(my_time, 0, -1): # You could also use the reversed function, i.e: for x in reversed(range(0, my_time))
     seconds = x % 60
     minutes = int(x / 60) % 60
-    print(f"00:{minutes:02}:{seconds:02}")
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
     
 print("Time's Up")
-
