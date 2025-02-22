bfd79f265016c8a53a6c12a29923f54eff12e3ed
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-21 22:45:36 -0800

    Adding seconds to digital clock

--- projects/countdown_timer.py
+++ projects/countdown_timer.py
@@ -3,7 +3,9 @@ import time
 my_time = int(input("Enter the time in seconds: "))
 
 for x in range(my_time, 0, -1): # You could also use the reversed function, i.e: for x in reversed(range(0, my_time))
-    print(x)
-    time.sleep(2)
-
+    seconds = x % 60
+    print(f"00:00:{seconds:02}")
+    time.sleep(1)
+    
 print("Time's Up")
+
