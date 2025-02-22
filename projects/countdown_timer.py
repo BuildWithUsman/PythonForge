d92f9a7787299abecd3b49e747e90a4bf27c7167
BuildWithUsman <usman.mehraj1+github@gmail.com>
2025-02-21 22:34:29 -0800

    Basic Reverse Countdown Timer

--- projects/countdown_timer.py
+++ projects/countdown_timer.py
@@ -2,7 +2,7 @@ import time
 
 my_time = int(input("Enter the time in seconds: "))
 
-for x in range(my_time, 0, -1):
+for x in range(my_time, 0, -1): # You could also use the reversed function, i.e: for x in reversed(range(0, my_time))
     print(x)
     time.sleep(2)
 
